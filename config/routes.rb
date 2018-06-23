a65592be6a8e56eee19087f1ae7f17bf05942986
Siddhant Bhardwaj <siddhant.bhardwaj16@gmail.com>
2018-06-23 18:11:02 -0300

    changed routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,5 @@ Rails.application.routes.draw do
       get :images
     end
   end
-  
-  
-  upload_image_student POST   /students/:id/upload_image(.:format) students#upload_image
-        images_student GET    /students/:id/images(.:format)       students#images
-              students GET    /students(.:format)                  students#index
-                       POST   /students(.:format)                  students#create
-               student DELETE /students/:id(.:format)              students#destroy
 
 end
