53ecf58672e944bb0aa1b7a4106cc53a15d92295
Siddhant Bhardwaj <siddhant.bhardwaj16@gmail.com>
2018-06-24 00:48:11 -0300

    root endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  root to: lambda {|env| [404, {}, ['{"Cloud Assignment3": "Please use the correct api endpoints"}']] }, via: 'get'
   resources :students, only: [:index, :create, :destroy] do
     member do
       post :upload_image
