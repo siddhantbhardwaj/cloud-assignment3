68418a0e86a9299c506bdf40d977d5c3aca7831f
Siddhant Bhardwaj <siddhant.bhardwaj16@gmail.com>
2018-06-24 00:43:49 -0300

    handle wrong routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   resources :students, only: [:index, :create, :destroy] do
     member do
       post :upload_image
       get :images
     end
   end
+  
+  match '*wrong', via: 'get', to: lambda {|env| [404, {}, ['{"error": "Please make sure it is correct endpoint"}']] }
 
 end
