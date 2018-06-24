8200a912409e73b160bc0d0bd7288a6afaa102d0
Siddhant Bhardwaj <siddhant.bhardwaj16@gmail.com>
2018-06-24 01:14:01 -0300

    support delete http requests

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Assignment3
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :options, :delete]
       end
     end
     
